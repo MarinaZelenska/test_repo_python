e5cd79b997bd99ab1c650e6c8c5e19c7ef6aaad2
MarynaZelenska <osychenkomarina@gmail.com>
2024-03-15 19:53:44 +0200

    test

--- work_with_str.py
+++ work_with_str.py
@@ -1,4 +1,4 @@
 first = input("Enter first str: ")
-second = input("Enter second str: ")
+fourth = input("Enter second str: ")
 
-print(f'Is str the same? {first == second}')
+print(f'Is str the same? {first == fourth}')
